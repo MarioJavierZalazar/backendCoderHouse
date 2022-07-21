be162f075a8bfdbcf7d95eccbe6e9afcabb4ef3c
Javier Zalazar <mariojavierzalazar@gmail.com>
2022-07-21 18:58:15 -0300

    Se realiza desafio 2

---
+++ segundoDesafio/clases.js
@@ -0,0 +1,111 @@
+const fs = require('fs');
+
+class Container {
+    constructor(name) {
+        this.name = `./${name}.txt`
+    }
+
+    async save(objet) {
+        if (fileExists(this.name)) {
+            try {
+                let file = await fs.promises.readFile(this.name, 'utf-8')
+                let products = JSON.parse(file)
+                let lastItem = products[products.length - 1].id + 1;
+                objet.id = lastItem;
+                products.push(objet)
+                await fs.promises.writeFile(this.name, JSON.stringify(products))
+                console.log(objet.id);
+            } catch (error) {
+                throw new Error('Error al guardar')
+            }
+
+        } else {
+            objet.id = 1;
+            let newProduct = [];
+            newProduct.push(objet)
+            try {
+                await fs.promises.writeFile(this.name, JSON.stringify(newProduct))
+                console.log(objet.id);
+
+            } catch (error) {
+                throw new Error('Error al guardar')
+            }
+        }
+    }
+
+    async getById(id) {
+        if (fileExists(this.name)) {
+            try {
+                let file = await fs.promises.readFile(this.name, 'utf-8');
+                let products = JSON.parse(file);
+                let found = products.find(product => product.id === id);
+                console.log(found);
+            } catch (error) {
+                throw new Error('No se encuentra el ID indicado')
+            }
+        } else {
+            console.log('No existe el arvhico que esta buscando');
+        }
+    }
+
+    async getAll() {
+        if (fileExists(this.name)) {
+            try {
+                let file = await fs.promises.readFile(this.name, 'utf-8');
+                let products = JSON.parse(file);
+                console.log(products);
+            } catch (error) {
+                throw new Error('Error al leer el archivo')
+            }
+        } else {
+            console.log('No existe el arvhico que esta buscando');
+        }
+    }
+
+    async deleteById(id) {
+        if (fileExists(this.name)) {
+            try {
+                let file = await fs.promises.readFile(this.name, 'utf-8');
+                let products = JSON.parse(file);
+                let productDeleted = products.findIndex(product => product.id === id);
+                products.splice(productDeleted, 1);
+                await fs.promises.writeFile(this.name,  JSON.stringify(products));
+            } catch (error) {
+                throw new Error('No se encuentra el ID indicado')
+            }
+        } else {
+            console.log('No existe el arvhico que esta buscando');
+        }
+    }
+
+    async deleteAll() {
+        if (fileExists(this.name)) {
+            try {
+                await fs.promises.unlink(this.name);
+            } catch (error) {
+                throw new Error('Error al borrar el archivo')
+            }
+        } else {
+            console.log('No existe el arvhico que esta buscando');
+        }
+    }
+}
+
+
+// Helpers
+const fileExists = (path) => {
+    try {
+        return fs.statSync(path).isFile();
+    } catch (err) {
+        return false;
+    }
+}
+
+const readFile = async (route) => {   
+        let file = await fs.promises.readFile(route, 'utf-8');
+        return JSON.parse(file);
+
+}
+
+
+exports.Container = Container;
---
+++ segundoDesafio/index.js
@@ -0,0 +1,27 @@
+const container = require('./clases');
+// Ejecucion del codigo
+
+const products = new container.Container('productos')
+
+
+// Agregamos un producto nuevo y se crea el archivo
+// products.save({title: 'Escuadra',price: 123.45, thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png'});
+
+// sumamos varios productos al arvhico ya creado
+// Producto 2
+//  products.save({title: 'Calculadora', price: 234.56, thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png'});
+// Producto 3
+//  products.save({title: 'Globo Terráqueo', price: 345.67, thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png'});
+
+//obtenemos un producto por ID
+// products.getById(2);
+
+//obtenemos todos los productos
+// products.getAll();
+
+//Eliminamos un producto por id
+// products.deleteById(2);
+
+// Borramos todos los productos y archivo
+// products.deleteAll();
+
